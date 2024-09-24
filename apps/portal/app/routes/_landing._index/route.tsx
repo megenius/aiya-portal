2a79fdb93eb9676497467e9b66933bd96665a996
Atchariya Darote <atchariya@gmail.com>
2024-09-24 15:55:03 +0700

    try speed

--- apps/portal/app/routes/_landing._index/route.tsx
+++ apps/portal/app/routes/_landing._index/route.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from "react"
+import { ClientOnly } from "remix-utils/client-only"
 import MainContent from "./_components/MainContent"
 
 const Route = () => {
   return (
     <>
-      <div className="p-2 sm:p-5 sm:py-0 md:pt-5 space-y-5">
-        <MainContent />
-      </div>
+      <Suspense fallback="">
+        <ClientOnly>
+          {() =>
+            <div className="p-2 sm:p-5 sm:py-0 md:pt-5 space-y-5">
+              <MainContent />
+            </div>
+          }
+        </ClientOnly>
+      </Suspense>
     </>
   )
 }
