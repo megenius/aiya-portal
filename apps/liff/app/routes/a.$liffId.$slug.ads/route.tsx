7d6ef20117cc58e5dd46fb66e04b399fa48fec80
Atchariya Darote <atchariya@gmail.com>
2024-10-06 18:33:10 +0700

    add preline

--- apps/liff/app/routes/a.$liffId.$slug.ads/route.tsx
+++ apps/liff/app/routes/a.$liffId.$slug.ads/route.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useOutletContext } from '@remix-run/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PageLiff } from '~/types/page';
 
 interface routeProps {
@@ -8,6 +8,20 @@ interface routeProps {
 
 const route: React.FC<routeProps> = () => {
   const { page } = useOutletContext<{ page: PageLiff }>()
+
+
+  useEffect(() => {
+    async function initializePreline() {
+      const { HSStaticMethods, HSAccordion, HSOverlay, HSTabs } = await import('preline/preline');
+      setTimeout(() => {
+        HSStaticMethods.autoInit();
+        HSOverlay.autoInit();
+        HSTabs.autoInit();
+      }, 500);
+    }
+
+    initializePreline();
+  }, [page]);
 
   return (
     <>
