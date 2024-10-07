2e45a3bb70088702a7dd23fb34460a0178f0adc9
Atchariya Darote <atchariya@gmail.com>
2024-10-08 00:02:11 +0700

    enable liff-api cache

--- apps/liff-api/src/index.ts
+++ apps/liff-api/src/index.ts
@@ -7,13 +7,13 @@ import { voucherRoutes } from "./routes/voucher";
 
 const app = new Hono<Env>()
   .basePath("/api")
-  // .get(
-  //   "*",
-  //   cache({
-  //     cacheName: "my-app",
-  //     cacheControl: "max-age=3600",
-  //   })
-  // )
+  .get(
+    "*",
+    cache({
+      cacheName: "my-app",
+      cacheControl: "max-age=3600",
+    })
+  )
   .route("/liff", liffRoutes)
   .route("/vouchers", voucherRoutes);
 
