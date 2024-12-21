8a8eded18e849b9125c59cb121128f350c21deaa
Atchariya Darote <atchariya@gmail.com>
2024-12-21 15:25:57 +0700

    release preview

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-bun dev --filter=portal --filter=portal-api --filter=bot-api --filter=channel-api --filter=ad-api --filter=customer-api --filter=shop-api --filter=partner-api --filter=facebook-proxy --filter=ai-api --filter=affiliate-api --concurrency 12
+bun dev --filter=portal --filter=portal-api --filter=bot-api --filter=channel-api --filter=ad-api --filter=customer-api --filter=shop-api --filter=partner-api --filter=facebook-proxy --filter=ai-api --filter=referral-api --concurrency 12
