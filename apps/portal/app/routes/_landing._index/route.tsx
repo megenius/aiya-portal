07f7e8e0cdffe06b89161b8ba9ee43880235d03b
Atchariya Darote <atchariya@gmail.com>
2024-12-29 20:41:44 +0700

    test

--- apps/portal/app/routes/_landing._index/route.tsx
+++ apps/portal/app/routes/_landing._index/route.tsx
@@ -1,20 +1,12 @@
 import React from "react"
-import { Suspense } from "react"
-import { ClientOnly } from "remix-utils/client-only"
 const MainContent = React.lazy(() => import("./_components/MainContent"))
 
 const Route = () => {
   return (
     <>
-      <Suspense fallback="">
-        <ClientOnly>
-          {() =>
-            <div className="p-2 sm:p-5 sm:py-0 md:pt-5 space-y-5">
-              <MainContent />
-            </div>
-          }
-        </ClientOnly>
-      </Suspense>
+      <div className="p-2 sm:p-5 sm:py-0 md:pt-5 space-y-5">
+        <MainContent />
+      </div>
     </>
   )
 }
