bde33e02bd9cfc73efc4508675e218242f8c02b0
Atchariya Darote <atchariya@gmail.com>
2025-01-16 05:45:04 +0700

    add webp

--- apps/website/app/routes/_landing.webinars.$slug/route.tsx
+++ apps/website/app/routes/_landing.webinars.$slug/route.tsx
@@ -14,20 +14,55 @@ export const meta: MetaFunction<typeof loader> = ({
   const webinar = data?.webinar
 
   // Basic meta tags
+  // const metaTags = [
+  //   // Additional tags from webinar metadata if they exist
+  //   ...(webinar?.metadata || []),
+
+  //   { title: webinar?.name || 'Webinar' },
+  //   { description: webinar?.description || 'Join our webinar' },
+
+  //   // Essential Open Graph tags
+  //   { property: 'og:title', content: webinar?.name },
+  //   { property: 'og:description', content: webinar?.description },
+  //   { property: 'og:type', content: 'website' },
+  //   { property: 'og:url', content: url },
+  //   { property: 'og:image', content: webinar?.cover },
+  // ];
+
+
   const metaTags = [
-    // Additional tags from webinar metadata if they exist
-    ...(webinar?.metadata || []),
-
-    { title: webinar?.name || 'Webinar' },
-    { description: webinar?.description || 'Join our webinar' },
-
-    // Essential Open Graph tags
-    { property: 'og:title', content: webinar?.name },
-    { property: 'og:description', content: webinar?.description },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: url },
-    { property: 'og:image', content: webinar?.cover },
-  ];
+    {
+      "title": "AI-Powered Sales Growth: The Art of Smart Customer Segmentation"
+    },
+    {
+      "name": "description",
+      "content": "ห้ามพลาด! AI Sales Webinar สุดพิเศษ รับฟรี Voucher 300 บาท เรียนรู้การใช้ AI เพิ่มยอดขายธุรกิจ"
+    },
+    {
+      "name": "keywords",
+      "content": "AI sales, customer segmentation, digital marketing, business growth, AIYA.AI, RFM model, Thai SME, AI marketing"
+    },
+    {
+      "property": "og:title",
+      "content": "ห้ามพลาด! รับฟรี Voucher 300 บาท AI Sales Webinar สุดพิเศษ"
+    },
+    {
+      "property": "og:description",
+      "content": "เรียนรู้การใช้ AI วิเคราะห์และแบ่งกลุ่มลูกค้าอย่างแม่นยำกับ CEO AIYA.AI และ LINE Certified Coach"
+    },
+    {
+      "property": 'og:image',
+      "content": "https://aiya.me/images/8d5538df-4ec2-49c4-9de6-589e371ce620.jpg" //webinar?.cover
+    },
+    {
+      "property": 'og:type',
+      "content": 'website'
+    },
+    {
+      "property": 'og:url',
+      "content": url
+    },
+  ]
 
   return metaTags;
 }
