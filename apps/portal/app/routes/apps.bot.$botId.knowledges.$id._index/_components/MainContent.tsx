5194dc891c66ad8182bfbfd006fb4f4c475619ad
Atchariya Darote <atchariya@gmail.com>
2025-01-25 06:57:28 +0700

    add gen waiting modal

--- apps/portal/app/routes/apps.bot.$botId.knowledges.$id._index/_components/MainContent.tsx
+++ apps/portal/app/routes/apps.bot.$botId.knowledges.$id._index/_components/MainContent.tsx
@@ -481,7 +481,7 @@ const ImportButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
 
 const GenerateWaitModal: React.FC<{}> = ({ }) => {
   return (
-    <div id="hs-static-backdrop-modal" className="hs-overlay [--overlay-backdrop:static] hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-static-backdrop-modal-label" data-hs-overlay-keyboard="false">
+    <div id="hs-static-backdrop-modal" className="hs-overlay hs-overlay-backdrop-open:bg-blue-950/90  [--overlay-backdrop:static] hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-static-backdrop-modal-label" data-hs-overlay-keyboard="false">
       <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500  mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
         <div className="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
           <div className="flex justify-between items-center py-3 px-4 border-b">
