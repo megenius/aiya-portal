ec0208e90a4bc9733bdac4bbcc5b55267cefb5f2
supachai-Aiya <supachai@aiya.ai>
2025-01-30 09:59:54 +0700

    fix : get contacts

--- apps/bot-api/src/routes/insights.ts
+++ apps/bot-api/src/routes/insights.ts
@@ -5,7 +5,7 @@ import * as InsightHandler from "../handlers/insights.handler";
 const insightsRoutes = new Hono<Env>();
 
 insightsRoutes.get("/logs", ...InsightHandler.getLogsHandler);
-insightsRoutes.get("/logs/latest", ...InsightHandler.getLatestLogs)
+insightsRoutes.get("/contacts", ...InsightHandler.getLatestLogs)
 
 // 'Today', 'Last 7 days', 'Last 14 days', 'Last 30 days', 'Last Month'
 insightsRoutes.get("/stats/today", ...InsightHandler.getTodayStatsHandler);
