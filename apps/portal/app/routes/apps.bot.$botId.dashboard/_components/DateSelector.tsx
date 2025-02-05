955b718029bf85f5931a3d6b9fee77a0f2d3354a
teerat-aiya <teerat@aiya.ai>
2025-02-05 11:52:08 +0700

    feat: improve DateSelector add icon

--- apps/portal/app/routes/apps.bot.$botId.dashboard/_components/DateSelector.tsx
+++ apps/portal/app/routes/apps.bot.$botId.dashboard/_components/DateSelector.tsx
@@ -32,30 +32,28 @@ const DateSelector: React.FC<DateSelectorProps> = ({ onDateChange }) => {
   const handleSelectOption = (option: string) => {
     setSelectedOption(option);
     setIsOpen(false);
-  
-    let result: { timeUnit?: string; startDate?: string; endDate?: string } = {};
-  
+
+    let result: { timeUnit?: string; startDate?: string; endDate?: string } =
+      {};
+
     if (["Today", "This Month", "Last Month"].includes(option)) {
       result = {
         timeUnit:
           option === "Today"
             ? "day"
             : option === "This Month"
-            ? "month"
-            : "lastMonth",
+              ? "month"
+              : "lastMonth",
       };
     } else {
       let startDate, endDate;
       if (option === "Last 7 days") {
         startDate = addDays(new Date(), -7);
         endDate = endOfDay(new Date());
-        
-      }
-      else if (option === "Last 14 days") {
+      } else if (option === "Last 14 days") {
         startDate = addDays(new Date(), -14);
         endDate = endOfDay(new Date());
-      }
-      else if (option === "Last 30 days") {
+      } else if (option === "Last 30 days") {
         startDate = addDays(new Date(), -30);
         endDate = endOfDay(new Date());
       }
@@ -78,23 +76,47 @@ const DateSelector: React.FC<DateSelectorProps> = ({ onDateChange }) => {
         endDate: format(endDate!, "yyyy-MM-dd"),
       };
     }
-  
+
     onDateChange?.(result);
   };
-  
 
   return (
     <div className="hs-dropdown relative inline-flex">
       <button
         id="hs-dropdown-default"
         type="button"
-        className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+        className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
         aria-haspopup="menu"
         aria-expanded={isOpen ? "true" : "false"}
         aria-label="Dropdown"
         onClick={toggleDropdown}
       >
-        {selectedOption}
+          <svg
+            className="shrink-0 size-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <rect width={18} height={18} x={3} y={4} rx={2} ry={2} />
+            <line x1={16} x2={16} y1={2} y2={6} />
+            <line x1={8} x2={8} y1={2} y2={6} />
+            <line x1={3} x2={21} y1={10} y2={10} />
+            <path d="M8 14h.01" />
+            <path d="M12 14h.01" />
+            <path d="M16 14h.01" />
+            <path d="M8 18h.01" />
+            <path d="M12 18h.01" />
+            <path d="M16 18h.01" />
+          </svg>
+          {selectedOption}
+        
+
         <svg
           className={`hs-dropdown-open:rotate-180 size-4 ${isOpen ? "rotate-180" : ""}`}
           xmlns="http://www.w3.org/2000/svg"
@@ -113,7 +135,7 @@ const DateSelector: React.FC<DateSelectorProps> = ({ onDateChange }) => {
 
       {isOpen && (
         <ul
-          className="hs-dropdown-menu transition-[opacity,margin] duration-150 opacity-100 min-w-60 bg-white shadow-md rounded-lg mt-2 absolute right-0 top-full z-50 max-w-full"
+          className="p-1 hs-dropdown-menu transition-[opacity,margin] duration-150 opacity-100 min-w-[120px] w-full bg-white shadow-md rounded-lg mt-2 absolute right-0 top-full z-50"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="hs-dropdown-default"
