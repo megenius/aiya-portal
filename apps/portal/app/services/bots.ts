e1b56ad0affc9a6a7f6d1cb646e69dcc40033ef0
Atchariya Darote <atchariya@gmail.com>
2025-03-28 02:29:11 +0700

    auto create Playground channel

--- apps/portal/app/services/bots.ts
+++ apps/portal/app/services/bots.ts
@@ -37,6 +37,7 @@ export const deleteBot = (id: string) => {
   return api.delete(`/items/bots/${id}`);
 };
 
+
 // --------------- botModel ---------------
 export const fetchBotModels = () => api.get<Array<BotModel>>("/bots/models");
 
